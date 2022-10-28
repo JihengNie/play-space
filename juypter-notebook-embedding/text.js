633643182f52309cd0402f717c99855fe9efaee0
Jiheng Nie <Niejiheng@gmail.com>
2022-10-28 18:30:33 +0000

    Saving

---
+++ juypter-notebook-embedding/text.js
@@ -0,0 +1,25 @@
+var nodePandoc = require('node-pandoc');
+var src, args, callback;
+
+src = './sample.tex';
+
+// Arguments can be either a single string:
+// args = '-f docx -t markdown -o ./markdown.md';
+// Or in an array of strings -- careful no spaces are present:
+args = ['-f', 'latex', '-t', 'html', '-o', 'document.html'];
+
+// Set your callback function
+callback = function (err, result) {
+
+  if (err) {
+    console.error('Oh Nos: ', err);
+  }
+
+  // For output to files, the 'result' will be a boolean 'true'.
+  // Otherwise, the converted value will be returned.
+  console.log(result);
+  return result;
+};
+
+// Call pandoc
+nodePandoc(src, args, callback);
